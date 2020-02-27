1c086da22b847b174c96ecc31b35b6c739a94f06
ChristyMims <51426259+ChristyMims@users.noreply.github.com>
2020-02-27 15:59:16 -0600

    Add files via upload

---
+++ CapstoneProject1/app/src/main/java/com/example/capstoneproject1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.capstoneproject1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
